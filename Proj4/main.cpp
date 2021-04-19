1f318ce99010a14eefacec3a28b994ec032faee7
Fernando <faavanso@gmail.com>
2021-04-19 13:34:11 -0400

    changes

--- Proj4/main.cpp
+++ Proj4/main.cpp
@@ -11,7 +11,7 @@ int main() {
 
 	vector<BasicShape*> myVector;
 
-	for (double i = 0; i < 10; i++)
+	for (double i = 0; i < 3; i++)
 	{
 		int random;
 		random = rand() % 10;
@@ -23,23 +23,18 @@ int main() {
 	}
 
 	int size = myVector.size();
-	for (int i = 0; i < size; i++) {
 
-		myVector[i]->whatAmI();
+	/*cout << size << endl;
+	cout << myVector.at(1) << endl;*/
+
+	for (int i = 0; i < size; i++) {
+		
 		myVector[i]->getArea();
+		myVector[i]->whatAmI();
 
 
 		delete myVector[i];
 	}
-
-
-	/*In the main file, you should dynamically create the instances of the Rectanglesand Circles, storing pointers to
-		the objects into the vector<BasicShape*>.When you test it out in main, loop through the vector, and print out
-		the type of class (using the whatAmI) as well as the area, using getArea.You should get the correct values,
-		based on the type of object at each position within the area.
-		Also, when done, do not forget to delete the objects as well as clear the vector.*/
-
-
 
 	return 0;
 }
